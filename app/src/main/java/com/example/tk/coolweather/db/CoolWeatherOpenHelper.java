0e1d49301e1eacfd186d0129081d15b88689f659
[zhangpeifei12] <[229269553@qq.com]>
2017-10-31 23:22:28 +0800

    修改创建数据库错误

--- app/src/main/java/com/example/tk/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/example/tk/coolweather/db/CoolWeatherOpenHelper.java
@@ -15,7 +15,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
             + "province_code text)";
 
     public static final String CREATE_CITY = "create table City ("
-            + "in integer primary key autoincrement,"
+            + "id integer primary key autoincrement,"
             + "city_name text,"
             + "city_code text,"
             + "province_id integer)";
