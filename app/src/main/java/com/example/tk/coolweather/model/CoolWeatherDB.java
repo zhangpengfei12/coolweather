330c26b67661d55e682fb53b91c96741510e7367
zhangpengfei12 <229269553@qq.com>
2017-11-03 17:05:59 +0800

    修复实例化city时bug

--- app/src/main/java/com/example/tk/coolweather/model/CoolWeatherDB.java
+++ app/src/main/java/com/example/tk/coolweather/model/CoolWeatherDB.java
@@ -65,7 +65,7 @@ public class CoolWeatherDB {
             ContentValues values = new ContentValues();
             values.put("city_name",city.getCityName());
             values.put("city_code",city.getCityCode());
-            values.put("procince_id",city.getProvinceId());
+            values.put("province_id",city.getProvinceId());
             db.insert("City",null,values);
         }
     }
